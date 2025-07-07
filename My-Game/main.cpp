2d42b74a2b91393b8d87759b342f7388bc04e311
Abhishek <MLEngineer2024@proton.me>
2025-07-07 12:54:26 +0530

    Added player movement
    -Added player sprite
    -Added movement code for the player

--- My-Game/main.cpp
+++ My-Game/main.cpp
@@ -1,11 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include<iostream>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 400, 400 }), "SFML works!");
-    sf::CircleShape shape(200.f);
-    shape.setFillColor(sf::Color::Cyan);
-
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML shapes", sf::Style::Default, sf::State::Windowed, settings);
+    //---------------------------LOAD----------------------------------//
+    
+    sf::Texture texture("Assets/Player/Textures/ship.png");
+    sf::Sprite playerSprite(texture);
+    
+    //Main game loop 
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -13,9 +19,23 @@ int main()
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+        sf::Vector2f position = playerSprite.getPosition();
 
-        window.clear();
-        window.draw(shape);
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) 
+            playerSprite.setPosition(position + sf::Vector2f(0, -0.1));
+ 
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) 
+            playerSprite.setPosition(position + sf::Vector2f(0.1, 0));
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) 
+            playerSprite.setPosition(position + sf::Vector2f(0.0,0.1));
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) 
+            playerSprite.setPosition(position + sf::Vector2f(-0.1, 0));
+       
+      
+        window.clear(sf::Color::Black);
+        window.draw(playerSprite);
         window.display();
     }
 }
