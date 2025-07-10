3460518db1651308daafaf78c1bc14df08e680ec
Abhishek <MLEngineer2024@proton.me>
2025-07-10 13:39:06 +0530

    Bullets
    -Added an enemy sprite
    -Added the ability for the player to shoot the enemy

--- My-Game/main.cpp
+++ My-Game/main.cpp
@@ -1,17 +1,42 @@
 #include <SFML/Graphics.hpp>
 #include<iostream>
+#include<math.h>
 
+//--------------------------------------Normalize vector_________________________________
+sf::Vector2f NormalizedVector(sf::Vector2f vector) {
+    float mag = sqrt(vector.x * vector.x + vector.y * vector.y);
+    sf::Vector2f normalizedVector;
+    normalizedVector.x = vector.x / mag;
+    normalizedVector.y = vector.y / mag;
+    return normalizedVector;
+}
 int main()
 {
     sf::ContextSettings settings;
     settings.antiAliasingLevel = 8;
-    sf::RenderWindow window(sf::VideoMode({ 800, 600 }), "SFML shapes", sf::Style::Default, sf::State::Windowed, settings);
-    //---------------------------LOAD----------------------------------//
+    sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "SFML shapes", sf::Style::Default, sf::State::Windowed, settings);
+
     
+
+
+    //---------------------------LOAD----------------------------------//
     sf::Texture texture("Assets/Player/Textures/ship.png");
     sf::Sprite playerSprite(texture);
+    sf::Texture enemyTexture("Assets/wolf/Texture/Alien-Mothership2.png");
+    sf::Sprite enemySprite(enemyTexture);
+    playerSprite.setPosition(sf::Vector2f(1700, 700));
+
+
+    //-----------------------BULLET-----------------------------------
+   // sf::RectangleShape bullet(sf::Vector2f(25, 25));
+   // sf::Vector2f position = playerSprite.getPosition();
+   // bullet.setPosition(position);
+    float bulletSpeed = 1.f;
+    std::vector<sf::RectangleShape> bullets;
+    
     
     //Main game loop 
+    
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
@@ -19,34 +44,47 @@ int main()
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+ 
+        sf::Vector2f position1 = playerSprite.getPosition();
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-
-            sf::Vector2f position1 = playerSprite.getPosition();
-            playerSprite.setPosition(position1 + sf::Vector2f(0, -0.1));
+            
+            playerSprite.setPosition(position1 + sf::Vector2f(0, -2));
         }
  
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-            sf::Vector2f position2 = playerSprite.getPosition();
-            playerSprite.setPosition(position2 + sf::Vector2f(0.1, 0));
-
+            
+            playerSprite.setPosition(position1 + sf::Vector2f(2, 0));
         }
         
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            sf::Vector2f position3 = playerSprite.getPosition();
-            playerSprite.setPosition(position3 + sf::Vector2f(0.0,0.1));
-
+            playerSprite.setPosition(position1 + sf::Vector2f(0.0, 2));
         }
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            sf::Vector2f position4 = playerSprite.getPosition();
-
-            playerSprite.setPosition(position4 + sf::Vector2f(-0.1, 0));
+            playerSprite.setPosition(position1 + sf::Vector2f(-2, 0));
+        }
+        if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+            bullets.push_back(sf::RectangleShape(sf::Vector2f(20, 10)));
+            int lastElement = bullets.size() - 1;
+            bullets[lastElement].setPosition(playerSprite.getPosition());
+        }
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            sf::Vector2f bulletDirection = enemySprite.getPosition() - bullets[i].getPosition();
+            bulletDirection = NormalizedVector(bulletDirection);
+            bullets[i].setPosition(bullets[i].getPosition() + bulletDirection * bulletSpeed);
         }
        
       
         window.clear(sf::Color::Black);
         window.draw(playerSprite);
+        window.draw(enemySprite);
+        for (size_t i = 0; i < bullets.size(); i++)
+        {
+            window.draw(bullets[i]);
+
+        }
         window.display();
     }
 }
